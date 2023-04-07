f51797327479e263c56b8620049b923200d81c2e
rifqiluthfian <rluthfianofficial@gmail.com>
2023-04-07 16:37:18 +0700

    aweawe

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('.owl-carousel-atas').owlCarousel({
+        autoplay:true,
+        autoplayTimeout:5000,
+        dots:true,
+        dotsEach:true,
+        loop:true,
+
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                margin:3,
+                nav:false,
+                loop:true
+            }
+        }
+    })
+    $('.owl-carousel-bawah').owlCarousel({
+        dots:true,
+        dotsEach:true,
+        loop:true,
+        margin:30,
+        nav:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:5,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            },
+            1000:{
+                items:5,
+                margin:20,
+                nav:true,
+                loop:true
+            }
+        }
+    })
+  });
+// var myVar;
+
+// function myFunction() {
+//   myVar = setTimeout(showPage, 3000);
+// }
+
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
+
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+  
