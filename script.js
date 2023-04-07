eb4b6f7dc5ad7a2b1d0f3e563970c0ec9d8ffaa0
rifqiluthfian <rluthfianofficial@gmail.com>
2023-04-07 17:22:03 +0700

    asd

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
         responsiveClass:true,
         responsive:{
             0:{
-                items:5,
+                items:2,
                 nav:true
             },
             600:{
